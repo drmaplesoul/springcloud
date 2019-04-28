249244e49ff446deb9cb9c7174631ba5bc9484c2
drmaplesoul <185937320@qq.com>
2019-04-28 23:39:01 +0800

    redis

--- userserver/src/main/java/com/zdr/springcloud/userserver/UserServerApplication.java
+++ userserver/src/main/java/com/zdr/springcloud/userserver/UserServerApplication.java
@@ -4,9 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @MapperScan("com.zdr.springcloud.userserver.repositry")
 @EnableCaching
 public class UserServerApplication {
