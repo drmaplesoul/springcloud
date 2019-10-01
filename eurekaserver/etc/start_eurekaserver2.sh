8d6d0a3b169d7d9bf54ba752db7b9e4dadde9add
drmaplesoul <185937320@qq.com>
2019-09-30 21:04:15 -0700

    sh

---
+++ eurekaserver/etc/start_eurekaserver2.sh
@@ -0,0 +1,3 @@
+servername=eurekaserver2
+echo "-Dspring.profiles.active="$servername
+java -Dspring.profiles.active=$servername -jar ../target/eurekaserver-0.0.1-SNAPSHOT.jar>>../logs/$servername/app.log 2>&1 &
