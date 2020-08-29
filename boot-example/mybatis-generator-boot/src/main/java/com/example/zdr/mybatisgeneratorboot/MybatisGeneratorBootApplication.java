df74a1d663490c01b8c300656248ae101b7e5187
drmaplesoul <185937320@qq.com>
2020-08-29 20:05:32 +0800

    boot example

---
+++ boot-example/mybatis-generator-boot/src/main/java/com/example/zdr/mybatisgeneratorboot/MybatisGeneratorBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.zdr.mybatisgeneratorboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisGeneratorBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisGeneratorBootApplication.class, args);
+    }
+
+}
